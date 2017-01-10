9d4d107bc7862dc86c665b892ad92563be12bf79
Colin Woodbury <colingw@gmail.com>
2017-01-10 07:11:04 -0800

    Desimplify for speed
    - We get a slight speedup by using a `case` statement
    directly instead of `either`:
    
    (on `ireland.osm`)
    
    UNOPTIMIZED
    -----------
    21.58user 2.53system 0:27.18elapsed
    21.07user 2.52system 0:26.03elapsed
    21.86user 2.68system 0:26.91elapsed
    21.19user 2.51system 0:25.52elapsed
    21.25user 2.32system 0:24.60elapsed
    
    MEAN USER: 21.389999999999997
    MEAN ELAPSED: 26.048000000000002
    
    OPTIMIZED
    ---------
    21.08user 2.46system 0:24.51elapsed
    20.84user 2.58system 0:24.34elapsed
    21.88user 2.50system 0:25.57elapsed
    21.13user 2.37system 0:24.10elapsed
    21.22user 2.39system 0:24.53elapsed
    
    MEAN USER: 21.229999999999997
    MEAN ELAPSED: 24.610000000000003

--- Axe.hs
+++ Axe.hs
@@ -14,7 +14,7 @@ import           Text.Printf
 type Line = BL.ByteString
 
 file :: FilePath
-file = "/home/colin/code/azavea/axe/north-van.osm"
+file = "/home/colin/code/azavea/axe/ireland.osm"
 
 -- | The output path.
 out :: Int -> FilePath
@@ -28,13 +28,17 @@ xml = S.drop 3 . S.mapped Q.toLazy . Q.lines $ Q.readFile file
 -- | Child tags begin with at least two whitespaces.
 isChild :: Line -> Bool
 isChild = BL.isPrefixOf "  "
+{-# INLINE isChild #-}
 
 -- | Stream a group of 10,000 or so lines, such that each Element's closing tag
 -- is present in the Stream. Returns the rest of the Stream.
 elements :: Monad m => Stream (Of Line) m r -> Stream (Of Line) m (Stream (Of Line) m r)
-elements s = S.splitAt 10000 s >>= S.span isChild >>= lift . S.next >>= either (pure . pure) f
-  where f (e,s') | BL.isPrefixOf " </" e = S.yield e >> pure s'
-                 | otherwise = pure $ S.cons e s'
+elements s = do
+  next <- S.splitAt 10000 s >>= S.span isChild >>= lift . S.next
+  case next of
+    Right (e,s') | BL.isPrefixOf " </" e -> S.yield e >> pure s'
+                 | otherwise -> pure $ S.cons e s'
+    Left r -> pure $ pure r
 
 -- | Yield a legal @<osm> ... </osm>@ block.
 osm :: Monad m => Stream (Of Line) m r -> Stream (Of Line) m (Stream (Of Line) m r)
@@ -43,8 +47,10 @@ osm s = (\_ r _ -> r) <$> S.yield "<osm>" <*> elements s <*> S.yield "</osm>"
 -- | Write each @<osm>@ block to a separate file.
 work :: MonadResource m => Int -> Stream (Of Line) m r -> m ()
 work !n s = do
-  rest <- Q.writeFile (out n) . Q.unlines $ S.with (osm s) Q.fromLazy
-  S.next rest >>= either (\_ -> pure ()) (\(e,s') -> work (n + 1) $ S.cons e s')
+  next <- (Q.writeFile (out n) . Q.unlines $ S.with (osm s) Q.fromLazy) >>= S.next
+  case next of
+    Right (e,s') -> work (n + 1) $ S.cons e s'
+    Left _ -> pure ()
 
 main :: IO ()
 main = runResourceT (work 0 xml) >> putStrLn "Done."
