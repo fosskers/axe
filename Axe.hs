a2e18034603f97992d003d535dc3f35e1a20f0cf
Colin Woodbury <colingw@gmail.com>
2017-01-10 14:02:44 -0800

    Added `--catalog` flag for specifying the output location

--- Axe.hs
+++ Axe.hs
@@ -9,6 +9,7 @@ import qualified Data.ByteString.Streaming.Char8 as Q
 import           Options.Generic
 import           Streaming
 import qualified Streaming.Prelude as S
+import           System.FilePath.Posix ((</>))
 import           Text.Printf
 
 ---
@@ -16,11 +17,11 @@ import           Text.Printf
 type Line = BL.ByteString
 
 -- | The runtime environment.
-data Env = Env { input :: FilePath } deriving (Generic, ParseRecord)
+data Env = Env { input :: FilePath, catalog :: FilePath } deriving (Generic, ParseRecord)
 
 -- | The output path.
-out :: Int -> FilePath
-out = printf "/home/colin/code/azavea/axe/catalog/out-%08d.osm"
+out :: FilePath -> Int -> FilePath
+out fp = printf (fp </> "out-%08d.osm")
 
 -- | Streams elements from the source file line-by-line.
 -- This drops the first three lines, which are not Elements.
@@ -47,14 +48,14 @@ osm :: Monad m => Stream (Of Line) m r -> Stream (Of Line) m (Stream (Of Line) m
 osm s = (\_ r _ -> r) <$> S.yield "<osm>" <*> elements s <*> S.yield "</osm>"
 
 -- | Write each @<osm>@ block to a separate file.
-work :: MonadResource m => Int -> Stream (Of Line) m r -> m ()
-work !n s = do
-  next <- (Q.writeFile (out n) . Q.unlines $ S.with (osm s) Q.fromLazy) >>= S.next
+work :: MonadResource m => FilePath -> Int -> Stream (Of Line) m r -> m ()
+work c !n s = do
+  next <- (Q.writeFile (out c n) . Q.unlines $ S.with (osm s) Q.fromLazy) >>= S.next
   case next of
-    Right (e,s') -> work (n + 1) $ S.cons e s'
+    Right (e,s') -> work c (n + 1) $ S.cons e s'
     Left _ -> pure ()
 
 main :: IO ()
 main = do
-  Env i <- getRecord "Axe - Cut arbitrarily large OSM XML files."
-  runResourceT (work 0 $ xml i) >> putStrLn "Done."
+  Env i c <- getRecord "Axe - Cut arbitrarily large OSM XML files."
+  runResourceT (work c 0 $ xml i) >> putStrLn "Done."
