3e02dee45b8732e2dd7f28b21e254a0b0d88d87d
Colin Woodbury <colingw@gmail.com>
2017-01-10 05:47:34 -0800

    Simplifications

--- Axe.hs
+++ Axe.hs
@@ -32,30 +32,19 @@ isChild = BL.isPrefixOf "  "
 -- | Stream a group of 10,000 or so lines, such that each Element's closing tag
 -- is present in the Stream. Returns the rest of the Stream.
 elements :: Monad m => Stream (Of Line) m r -> Stream (Of Line) m (Stream (Of Line) m r)
-elements s = do
-  rest <- S.splitAt 10000 s >>= S.span isChild
-  next <- lift $ S.next rest
-  case next of
-    Right (e,s') | BL.isPrefixOf " </" e -> S.yield e >> pure s'
-                 | otherwise -> pure $ S.cons e s'
-    Left _ -> pure rest  -- An already empty Stream.
+elements s = S.splitAt 10000 s >>= S.span isChild >>= lift . S.next >>= either (pure . pure) f
+  where f (e,s') | BL.isPrefixOf " </" e = S.yield e >> pure s'
+                 | otherwise = pure $ S.cons e s'
 
 -- | Yield a legal @<osm> ... </osm>@ block.
 osm :: Monad m => Stream (Of Line) m r -> Stream (Of Line) m (Stream (Of Line) m r)
-osm s = do
-  S.yield "<osm>"
-  rest <- elements s
-  S.yield "</osm>"
-  pure rest
+osm s = (\_ r _ -> r) <$> S.yield "<osm>" <*> elements s <*> S.yield "</osm>"
 
 -- | Write each @<osm>@ block to a separate file.
 work :: MonadResource m => Int -> Stream (Of Line) m r -> m ()
 work !n s = do
   rest <- Q.writeFile (out n) . Q.unlines $ S.with (osm s) Q.fromLazy
-  next <- S.next rest
-  case next of
-    Left _ -> pure ()
-    Right (e,s') -> work (n + 1) $ S.cons e s'
+  S.next rest >>= either (\_ -> pure ()) (\(e,s') -> work (n + 1) $ S.cons e s')
 
 main :: IO ()
-main = (runResourceT $ work 0 xml) >> putStrLn "Done."
+main = runResourceT (work 0 xml) >> putStrLn "Done."
